f67ab8c5dbe150d53afd895c3aa5eedb4e69da20
OliverWong258 <3185336653@qq.com>
2026-08-10 20:03:20 -0700

    Longest Common Subsequence

---
+++ java-template/src/main/java/leetcode/editor/cn/LongestCommonSubsequence.java
@@ -0,0 +1,46 @@
+/*
+ * @lc app=leetcode.cn id=1143 lang=java
+ * @lcpr version=30404
+ *
+ * [1143] 最长公共子序列
+ */
+
+package leetcode.editor.cn;
+
+import java.util.*;
+import leetcode.editor.common.*;
+
+public class LongestCommonSubsequence {
+
+    // @lc code=start
+    class Solution {
+        public int longestCommonSubsequence(String text1, String text2) {
+            
+        }
+    }
+    // @lc code=end
+    
+    public static void main(String[] args) {
+        Solution solution = new LongestCommonSubsequence().new Solution();
+        // put your test code here
+        
+    }
+}
+
+
+
+/*
+// @lcpr case=start
+// "abcde"\n"ace"\n
+// @lcpr case=end
+
+// @lcpr case=start
+// "abc"\n"abc"\n
+// @lcpr case=end
+
+// @lcpr case=start
+// "abc"\n"def"\n
+// @lcpr case=end
+
+ */
+
